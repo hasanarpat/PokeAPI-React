77c03c54a06f87c7247bbc0a0d0ae04fbf3c256d
theakhilleus <35002572+theakhilleus@users.noreply.github.com>
2022-06-10 21:51:13 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
 function App() {
+    
+  const[pokeName,setPokeName]=useState(null);
+  const [pokeImg,setImg]=useState("images/pokeball.png");
+  const [text,setText]=useState(null);  
+  const [link,setLink]=useState(null);
+  const [name,setName]=useState(null);
+  function pikachu(event){
+    const name=event.target.value;
+    setPokeName(name);
+  }
+
+  async function bringPokemon(){
+    console.log(pokeName);
+    let url="https://pokeapi.co/api/v2/pokemon/"+pokeName;
+    const response = await fetch(url);
+    let data = await response.json();
+    const image=data.sprites.other.dream_world.front_default
+    const name=data.name;
+    const weight=data.weight;
+    setImg(image);
+    setName(name);
+    setText("Weight: "+weight);
+    setLink("Search for "+name);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{height: '100vh'}}>
+      <div className='main' style={{width:500}}>
+        <div className='search'>
+          <input placeholder='Enter an Pokemon Name...' onChange={pikachu}></input>
+          <br></br>
+          <button type='submit' onClick={bringPokemon}>Search</button>
+        </div>
+        <div className='card' >
+          <img className="card-img-top" src={pokeImg} alt="Card image cap"/>
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <p className="card-text">{text}</p>
+            <a href={"https://www.google.com/search?q="+pokeName} className="btn btn-primary">{link}</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
